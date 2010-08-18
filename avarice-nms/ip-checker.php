248e4d5413d90c8279e5fe143a9a6143007a19e8
richrumble <richrumble@1a33258e-1bfd-11df-83b7-07facc0237bd>
2010-08-18 13:17:58 +0000

    added GPL disclaimer and information to the php scripts.

--- avarice-nms/ip-checker.php
+++ avarice-nms/ip-checker.php
@@ -1,3 +1,27 @@
+/*
+ +--------------------------------------------------------------------------+
+ | Copyright (C) 2009-2010 Xinn.org                                         |
+ |                                                                          |
+ | This program is free software; you can redistribute it and/or            |
+ | modify it under the terms of the GNU General Public License              |
+ | as published by the Free Software Foundation; either version 2           |
+ | of the License, or (at your option) any later version.                   |
+ |                                                                          |
+ | This program is distributed in the hope that it will be useful,          |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            |
+ | GNU General Public License for more details.                             |
+ +--------------------------------------------------------------------------+
+ |Avarice-nms:A greedy and insatiable inventory and network managment system|
+ +--------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Xinn.org. See      |
+ | about.php and/or the AUTHORS file for specific developer information.    |
+ +--------------------------------------------------------------------------+
+ | http://avarice-nms.com                                                   |
+ | http://avarice-nms.info                                                  |
+ | http://xinn.org/avarice.php                                              |
+ +--------------------------------------------------------------------------+
+*/
 <?php
 $form_data = $_POST;
 function find ($string, $array = array ()) {
