94c53bec1c978177f12c7bbd6ba075a3fe8717e0
phreakbag@gmail.com <phreakbag@gmail.com@1a33258e-1bfd-11df-83b7-07facc0237bd>
2010-12-30 22:11:18 +0000

    Inventory coming along, but still not functional. Getting close.

--- avarice-nms/modules/invertory/inventory.php
+++ avarice-nms/modules/invertory/inventory.php
@@ -27,13 +27,37 @@
 
 include_once("../../include/config.php");
 
+function get_hash_ID($string) {
+  global $avarice_user_connection;
+  global $avarice_admin_connection;
+  $hash = hash('md5', $string);
+  $query = "
+            SELECT hash_ID
+              FROM inv__hash
+             WHERE hash = \"" . $hash . "\"";
+  $result = dbquery_func($avarice_user_connection, $query);
+  if (mysql_num_rows($result) == 1) {
+    return mysql_result($result, 0, 0);
+  } else {
+    $query = "
+              INSERT INTO inv__hash (hash)
+                   VALUES (\"" . $hash . "\")";
+    dbquery_func($avarice_admin_connection, $query);
+    $query = "
+              SELECT hash_ID
+                FROM inv__hash
+               WHERE hash = \"" . $hash . "\"";
+    return mysql_result(dbquery_func($avarice_user_connection, $query), 0, 0);
+  };
+};
+
 $process_time = gmdate('Y-m-d-H-i');
 
 $xml_path = mysql_result(dbquery_func($avarice_user_connection, "SELECT value FROM inv__config WHERE parameter = \"xml_path\""), 0, 0);
-$handle = opendir($xml_path)) {
+if ($handle = opendir($xml_path)) {
   $file_list = array();
   while (false !== ($file = readdir($handle))) {
-    if (!in_array($file, array(".", "..")) and is_file($xml_path . "/" . $file) and substr($file, -4) == ".xml") {{
+    if (!in_array($file, array(".", "..")) and is_file($xml_path . "/" . $file) and substr($file, -4) == ".xml") {
       $file_list[] = $file;
     };
   };
@@ -41,7 +65,31 @@ $handle = opendir($xml_path)) {
 };
 
 foreach ($file_list as $file) {
+  print "Starting file " . $file . "\n";
+  $xml = simplexml_load_file($xml_path . "/" . $file);
+  foreach($xml->Asset as $asset) {
+    $query = "
+              INSERT INTO inv__asset (Name)
+                   VALUES (\"" . $asset->Name . "\")";
+    dbquery_func($avarice_admin_connection, $query);
+    $asset_ID = mysql_result(dbquery_func($avarice_user_connection, "SELECT Asset_ID FROM inv__asset WHERE Name = \"" . $asset->Name . "\""), 0, 0);
+    foreach($asset->Drives as $drive) {
+      $hash_ID = get_hash_ID($drive->Description . $drive->DeviceID . $drive->Name . $drive->ProviderName);
+      $query = "
+                INSERT INTO inv__drives (Asset_ID, hash_ID, Description, DeviceID, Name, ProviderName)
+                     VALUES (\"" . $asset_ID . "\", \"" . $hash_ID . "\", \"" . $drive->Description . "\", \"" . $drive->DeviceID . "\", \"" . $drive->Name . "\", \"" . $drive->ProviderName . "\")";
+      dbquery_func($avarice_admin_connection, $query);
+    };
+  };
   
+  
+  
+  
+//  $gzhandle = gzopen($xml_path . "/" . $process_time . ".processed.gz", 'a9');
+//  gzwrite($gzhandle, file_get_contents($xml_path . "/" . $file));
+//  gzclose($gzhandle);
+//  unlink($xml_path . "/" . $file);
+  print "Finished file " . $file . "\n";
 };
 
 ?>
