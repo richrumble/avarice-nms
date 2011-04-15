5881a2acb73075b0e3a6354f3e1acced2c1f0aba
phreakbag@gmail.com <phreakbag@gmail.com@1a33258e-1bfd-11df-83b7-07facc0237bd>
2011-04-15 02:55:04 +0000

    Updated db_config.php
    Created file to take xml files in xml_path specified in inv__config_server and dump them to the process table.

---
+++ trunk/avarice-nms/modules/inventory/inv_filetodb.php
@@ -0,0 +1,19 @@
+<?php
+
+$result = dbquery_func($avarice_user_connection, "SELECT value FROM inv__config_server WHERE parameter = 'xml_path'");
+$xml_path_string = mysql_result($result, 0, 0);
+$xml_path = dir($xml_path_string);
+while (FALSE !== ($file = $xml_path->read())) {
+  if (if_file($xml_path . "/" . $file) and (is_numeric(substr($file, 0, 8)) and substr($file, -4) == ".xml")) {
+    $query = "
+              INSERT INTO inv__dataprocessing
+                          (createdDate, assetName, data)
+                   VALUES ('" . substr($file, 0, 4) . "-" . substr($file, 4, 2) . "-" . substr($file, 6, 2) . "', '" . substr($file, 8, -4) . "', '" . file_get_contents($file) . "');";
+    dbquery_func($avarice_admin_connection, $query);
+    if (mysql_affected_rows() > 0) {
+      unlink($xml_path_string . "/" . $file);
+    };
+  };
+};
+
+?>
