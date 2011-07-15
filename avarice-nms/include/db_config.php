b90888cf6f8bec223e372f4a29c123ad91b8d517
phreakbag@gmail.com <phreakbag@gmail.com@1a33258e-1bfd-11df-83b7-07facc0237bd>
2011-07-15 02:37:04 +0000

    dbquery_func_new utilizes mysqli and prepared statements for security and efficiency purposes.  Need to run through all existing code to update the db calls to the correct format.

--- avarice-nms/include/db_config.php
+++ avarice-nms/include/db_config.php
@@ -43,4 +43,46 @@ function dbquery_func($connection_info, $query, $debug="off") {
   return $return;
 };
 
+function dbquery_func_new($connection_info, $query, $debug) {
+  if ($connection_info['db_type'] == "mysql") {
+    if (!is_array($query)) {
+      return FALSE;
+    };
+    $link = new mysqli($connection_info['db_host'], $connection_info['username'], $connection_info['password'], $connection_info['db_name'], $connection_info['db_port']);
+    if ($link->connect_error) {
+      die("Connection Error (" . $mysqli->connect_errno . ") - " . $mysqli->connect_error);
+    };
+    if ($stmt = $link->prepare($query['query'])) {
+      call_user_func_array(array($stmt, 'bind_param'), refvalues($query['params']));
+      $stmt->execute();
+      $meta = $stmt->result_metadata();
+      $parameters = array(); $results = array();
+      while ($field = $meta->fetch_field()) {
+        $parameters[] = &$row[$field->name];
+      };
+      call_user_func_array(array($stmt, 'bind_result'), refvalues($parameters));
+      while ($stmt->fetch()) {
+       $x = array();
+       foreach ($row as $key => $val) {
+          $x[$key] = $val;
+       };
+       $results[] = $x;
+      };
+      $stmt->close();
+      $mysqli->close();
+      return $result;
+    };
+  };
+
+};
+function refvalues($values){
+  if (strnatcmp(phpversion(),'5.3') >= 0) {
+    $refs = array();
+    foreach($values as $key => $value)
+      $refs[$key] = &$values[$key];
+    return $refs;
+  };
+  return $values;
+};
+
 ?>
