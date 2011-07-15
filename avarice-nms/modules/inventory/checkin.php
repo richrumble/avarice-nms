6101d8df91c28a5b7c07e26f29eaac3735e0c7fa
phreakbag@gmail.com <phreakbag@gmail.com@1a33258e-1bfd-11df-83b7-07facc0237bd>
2011-07-15 02:56:12 +0000

    More dbquery_func reworking.

--- avarice-nms/modules/inventory/checkin.php
+++ avarice-nms/modules/inventory/checkin.php
@@ -5,24 +5,33 @@ include_once("inv_functions.php");
 include_once("inv_config.php");
 
 if (!empty($form_data['action'])) {
+  $query = array();
   if ($form_data['action'] == "templatecheck") {
     $given_hash_id = getHashID($form_data['hash']);
-    $query = "
-              SELECT hash_ID
-                   , template
-                FROM inv__config_templates
-               WHERE inv__config_templates.os = '" . $form_data['os'] . "'
-                 AND inv__config_templates.release = '" . $form_data['release'] . "'
-                 AND inv__config_templates.version = '" . $form_data['version'] . "'";
-    $result = mysql_fetch_assoc(dbquery_func($avarice_user_connection, $query));
-    if ($given_hash_id != $result['hash_ID']) {
-      print $result['template'];
+    $query['query'] = "
+                       SELECT hash_ID
+                            , template
+                         FROM inv__config_templates
+                        WHERE inv__config_templates.os = ?
+                          AND inv__config_templates.release = ?
+                          AND inv__config_templates.version = ?";
+    $query['params'] = array("sss",
+                             $form_data['os'],
+                             $form_data['release'],
+                             $form_data['version']);
+    $result = dbquery_func($avarice_user_connection, $query);
+    if ($given_hash_id != $result[0]['hash_ID']) {
+      print $result[0]['template'];
     };
   } else if ($form_data['action'] == "submit_results") {
-    $query = "
-              INSERT INTO inv__dataprocessing
-                          (createdDate, assetName, data)
-                   VALUES ('" . substr($form_data['filename'], 0, 4) . "-" . substr($form_data['filename'], 4, 2) . "-" . substr($form_data['filename'], 6, 2) . "', '" . substr($form_data['filename'], 8, -4) . "', '" . $form_data['xml_result'] . "');";
+    $query['query'] = "
+                       INSERT INTO inv__dataprocessing
+                                   (createdDate, assetName, data)
+                            VALUES (?, ?, ?);";
+    $query['params'] = array("ssb",
+                             substr($form_data['filename'], 0, 4) . "-" . substr($form_data['filename'], 4, 2) . "-" . substr($form_data['filename'], 6, 2),
+                             substr($form_data['filename'], 8, -4),
+                             $form_data['xml_result']);
     dbquery_func($avarice_admin_connection, $query);
   };
 };
