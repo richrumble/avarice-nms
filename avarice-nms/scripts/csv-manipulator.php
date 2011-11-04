3d2b350c06194f93986fa0a9aba67a86729fe5c1
phreakbag@gmail.com <phreakbag@gmail.com@1a33258e-1bfd-11df-83b7-07facc0237bd>
2011-11-04 02:50:20 +0000

    csv-manipulator.php is now completely prepped for using a MySQL DB.  New error function, additional parameters now possible (for DB functions). Multiple files (greater than 2) are theoretically possible (not tested). All work now will be in lines greater than 181 for DB portion.

--- avarice-nms/scripts/csv-manipulator.php
+++ avarice-nms/scripts/csv-manipulator.php
@@ -1,58 +1,188 @@
 <?php
 
-if (empty($argv[1])) {
-  exit("You must provide a function: php csv-manipulator.php -sc \\path\\to\\file1.csv \\path\\to\\file2.csv\n  Available functions s = sort, c = compare\n");
+function throw_error($etype, $data = NULL) {
+  $gen_message  = "\n";
+  $gen_message .= "USAGE: php csv-manipulator.php [-options] \\path\\to\\file1.csv [\\path\\to\\file2.csv]\n";
+  $gen_message .= "\t-s:\t\tSort given file(s)\n";
+  $gen_message .= "\t-c:\t\tCompare given files\n";
+  $gen_message .= "\t-f:\t\tFile Based. Writes results to files. (Default)\n";
+  $gen_message .= "\t-d:\t\tDB Based. Writes results to DB.\n";
+  $gen_message .= "\t-db:[dbname]:\tDB Based. Connects to MySQL DB and results are inserted into tables. Default: avarice_nms\n";
+  $gen_message .= "\t-ho:[dbhost]:\tFQDN of MySQL DB host. Default: localhost\n";
+  $gen_message .= "\t-po:[dbport]:\tTCP Port to talk to MySQL DB. Default: 3306\n";
+  $gen_message .= "\t-un:[username]:\tMySQL DB user (needs table create / delete access on given MySQL DB. Default: root\n";
+  $gen_message .= "\t-pw:[password]:\tMySQL DB password. Default: NULL\n";
+  if ($etype == "no params") {
+    $message = "No parameters given.\n";
+  } else if ($etype == "invalid param") {
+    $message = "\"" . $data . "\" is not a valid parameter.\n";
+  } else if ($etype == "no file") {
+    $message = "Cannot access \"" . $data . "\".\n";
+  } else if ($etype == "file create") {
+    $message = "Could not open or create " . $data . ".\n";
+  } else if ($etype == "file open") {
+    $message = "Could not open " . $data . ".\n";
+  };
+  exit("\n" . $message . $gen_message, 1);
+};
+
+function csv_sort_fb($filenames) {
+  foreach ($filenames as $filename) {
+    if (($handle = fopen($filename, "r")) !== FALSE) {
+      $header_array = array(); $row = 1;
+      while (($data = fgetcsv($handle)) !== FALSE) {
+        $temp_string = "";
+        if ($row == 1) {
+          $header_array = $data;
+          asort($header_array);
+          if (($handle_out = fopen(substr($filename, 0, -4) . "-sorted.csv", "w")) === FALSE) {
+            throw_error("file create", substr($filename, 0, -4) . "-sorted.csv");
+          };
+          $row++;
+        };
+        foreach ($header_array as $key => $discard) {
+          $temp_string .= "\"" . $data[$key] . "\",";
+        };
+        fwrite($handle_out, substr($temp_string, 0, -1) . "\n");
+      };
+      fclose($handle_out);
+    } else {
+      throw_error("file open", $filename);
+    };
+    fclose($handle);
+  };
+};
+
+function csv_sort_db($filenames, $dbconnection) {
+  foreach ($filenames as $filename) {
+    if (($handle = fopen($filename, "r")) !== FALSE) {
+      $header_array = array(); $row = 1;
+      while (($data = fgetcsv($handle)) !== FALSE) {
+        $temp_string = "";
+        if ($row == 1) {
+          $header_array = $data;
+          asort($header_array);
+          if (($handle_out = fopen(substr($filename, 0, -4) . "-sorted.csv", "w")) === FALSE) {
+            throw_error("file create", substr($filename, 0, -4) . "-sorted.csv");
+          };
+          $row++;
+        };
+        foreach ($header_array as $key => $discard) {
+          $temp_string .= "\"" . $data[$key] . "\",";
+        };
+        fwrite($handle_out, substr($temp_string, 0, -1) . "\n");
+      };
+      fclose($handle_out);
+    } else {
+      throw_error("file open", $filename);
+    };
+    fclose($handle);
+  };
+};
+
+function dbquery_func($connection_info, $query, $debug) {
+  if ($connection_info['db_type'] == "mysql") {
+    if (!is_array($query)) {
+      return FALSE;
+    };
+    $link = new mysqli($connection_info['db_host'], $connection_info['username'], $connection_info['password'], $connection_info['db_name'], $connection_info['db_port']);
+    if ($link->connect_error) {
+      die("Connection Error (" . $mysqli->connect_errno . ") - " . $mysqli->connect_error);
+    };
+    if ($stmt = $link->prepare($query['query'])) {
+      call_user_func_array(array($stmt, 'bind_param'), refvalues($query['params']));
+      $stmt->execute();
+      $meta = $stmt->result_metadata();
+      $parameters = array(); $results = array();
+      while ($field = $meta->fetch_field()) {
+        $parameters[] = &$row[$field->name];
+      };
+      call_user_func_array(array($stmt, 'bind_result'), refvalues($parameters));
+      while ($stmt->fetch()) {
+       $x = array();
+       foreach ($row as $key => $val) {
+          $x[$key] = $val;
+       };
+       $results[] = $x;
+      };
+      $stmt->close();
+      $mysqli->close();
+      return $result;
+    };
+  };
+};
+
+function refvalues($values){
+  if (strnatcmp(phpversion(),'5.3') >= 0) {
+    $refs = array();
+    foreach($values as $key => $value)
+      $refs[$key] = &$values[$key];
+    return $refs;
+  };
+  return $values;
+};
+
+$params = array("s" => "sort",
+                "c" => "compare",
+                "f" => "file",
+                "d" => "db");
+
+$dbparams = array("db" => "db_name",
+                  "dt" => "db_type",
+                  "ho" => "db_host",
+                  "po" => "db_port",
+                  "un" => "username",
+                  "pw" => "password");
+
+$dbconnection = array("db_type"  => "mysql",
+                      "db_host"  => "localhost",
+                      "db_port"  => "3306",
+                      "db_name"  => "avarice_nms",
+                      "username" => "root",
+                      "password" => "");
+
+if (count($argv) == 1) {
+  throw_error("no params");
 } else {
-  $funcs = array();
-  if (strpos($argv[1], "s") !== FALSE or strpos($argv[1], "S") !== FALSE) {
-    $funcs[] = "sort";
-  };
-  if (strpos($argv[1], "c") !== FALSE or strpos($argv[1], "C") !== FALSE) {
-    $funcs[] = "compare";
-  };
-};
-
-if (empty($argv[2]) or !is_file($argv[2])) {
-  exit($argv[2] . " is not a file: php csv-manipulator.php -sc \\path\\to\\file1.csv \\path\\to\\file2.csv\n  Available functions s = sort, c = compare\n");
-};
-
-if (!empty($argv[3]) and !is_file($argv[3])) {
-  exit($argv[3] . " is not a file: php csv-manipulator.php -sc \\path\\to\\file1.csv \\path\\to\\file2.csv\n  Available functions s = sort, c = compare\n");
-} else if (!empty($argv[3]) and is_file($argv[3])){
-  $filename2 = $argv[3];
-  $output_filename2 = substr($argv[3], 0, -4) . "-sorted.csv";
-};
-
-function csv_sort($filename) {
-  if (($handle = fopen($filename, "r")) !== FALSE) {
-    $header_array = array(); $row = 1;
-    while (($data = fgetcsv($handle)) !== FALSE) {
-      $temp_string = "";
-      if ($row == 1) {
-        $header_array = $data;
-        asort($header_array);
-        if (($handle_out = fopen(substr($filename, 0, -4) . "-sorted.csv", "w")) === FALSE) {
-          exit("Could not open or create " . substr($filename, 0, -4) . "-sorted.csv" . ".\n");
+  $files = array();
+  $gparams = array();
+  foreach ($argv as $key => $value) {
+    if ($key != 0) {
+      if ($value[0] != "-") {
+        $files[] = $value;
+      } else {
+        if ($value[3] != ":") {
+          foreach(str_split(substr($value, 1)) as $char) {
+            if (in_array($char, array_keys($params))) {
+              $gparams[] = $params[$char];
+            } else {
+              throw_error("invalid param", $char);
+            };
+          };
+        } else {
+          if (in_array(substr($value, 1, 2), array_keys($dbparams))) {
+            $dbconnection[$dbparams[substr($value, 1, 2)]] = substr($value, 4);
+          } else {
+              throw_error("invalid param", substr($value, 1, 2));
+            };
+          };
         };
       };
-      foreach ($header_array as $key => $discard) {
-        $temp_string .= "\"" . $data[$key] . "\",";
-      };
-      fwrite($handle_out, substr($temp_string, 0, -1) . "\n");
-      $row++;
     };
-    fclose($handle_out);
-  } else {
-    exit("Could not open " . $filename . ".\n");
   };
-  fclose($handle);
 };
 
-if (in_array("sort", $funcs)) {
-  for ($x=2; $x<count($argv); $x++) {
-    if (is_file($argv[$x])) {
-      csv_sort($argv[$x]);
-    };
+foreach ($files as $file) {
+  if (!is_file($file)) {
+    throw_error("no file", $file);
+  };
+};
+
+if (in_array("sort", $gparams)) {
+  if (!in_array("db", $gparams)) {
+    csv_sort_fb($files);
+  } else {
+    csv_sort_db($files, $dbconnection);
   };
 };
 
@@ -65,8 +195,10 @@ if (in_array("compare", $funcs)) {
     $file1 = file($argv[3]);
   };
   
-  $matches = array_intersect($file1, $file2);
-  $diffs   = array_merge(array_diff($file1, $file2), array_diff($file2, $file1));
+  if (!in_array("db", $gparams)) {
+    $matches = array_intersect($file1, $file2);
+    $diffs   = array_merge(array_diff($file1, $file2), array_diff($file2, $file1));
+  };
   
   if (($handle = fopen("csv-compare.matches.csv", "w")) !== FALSE) {
     foreach ($matches as $line) {
