6d1fe4fe77ba0f81ab1314bf66e4b5f476fe213e
RichRumble@gmail.com <RichRumble@gmail.com@1a33258e-1bfd-11df-83b7-07facc0237bd>
2012-05-25 02:01:05 +0000

    adding a new highlight js file as well as some css updates

--- trunk/avarice-nms/logsearch.php
+++ trunk/avarice-nms/logsearch.php
@@ -9,7 +9,7 @@ if (empty($form_data['action'])) {
  <head>
   <title>Avarice 1.0</title>
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-  <link rel="stylesheet" href="css/style.css" type="text/css" />
+  <link rel="stylesheet" href="../css/style.css" type="text/css" />
   <link rel="icon" href="img/favicon.ico" type="img/x-icon" />
   <script type = "text/javascript" src = "jquery-1.6.1.min.js"></script>
   <script type = "text/javascript" src = "form.js"></script>
@@ -34,27 +34,38 @@ if (empty($form_data['action'])) {
    <b class="rtop">
     <b class="r0"></b> <b class="r1"></b> <b class="r2"></b> <b class="r3"></b> <b class="r4"></b>
    </b>
-	 <div class="cell">
+   <div class="cell">
     <form class = "formtodiv" targetdiv = "results" action = "logsearch.php" method = "POST">
      <fieldset>
-     	<legend>Event Log Search</legend>
-     	<label class="creds">FQDN or IP Target(s) | (csv)</label><input type=\"text\" name=\"fqdn\" />
-			<label class="creds">Username (your)</label><input type=\"text\" name=\"user\" />
-			<label class="creds">Password (your)</label><input type=\"password\" name=\"pass\" />
-	    <input type = "hidden" name = "action" value = "search" />
-	    <h3>Choose Log(s)</h3>
-		  <label><input type = "radio" name = "logfile" value = "all" checked />All&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-      <label><input type = "radio" name = "logfile" value = "Application" />Application</label>
-      <label><input type = "radio" name = "logfile" value = "Security" />Security</label>
-      <label><input type = "radio" name = "logfile" value = "System" />System</label>
+      <legend>Event Log Search</legend>
+      <label class="creds">FQDN or IP Target(s) | (csv)</label>
+       <input type=\"text\" name=\"fqdn\" /> <br />
+      <label class="creds">Username (domain\user)</label>
+       <input type=\"text\" name=\"user\" /> <br />
+      <label class="creds">Password </label>
+       <input type=\"password\" name=\"pass\" /> <br />
+      <input type = "hidden" name = "action" value = "search" />
+      <h3>Choose Log(s)</h3>
+      <label>
+       <input type = "radio" name = "logfile" value = "all" checked />All&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      </label>
+      <label>
+       <input class = "right" type = "radio" name = "logfile" value = "Application" />Application
+      </label><br />
+      <label>
+       <input type = "radio" name = "logfile" value = "Security" />Security
+      </label>
+      <label>
+       <input class = "right" type = "radio" name = "logfile" value = "System" />System
+      </label>
       <h3>Timeframe</h3>
       <label><input type = "radio" name = "timeframe" value = "-1 hour" checked />Last Hour&nbsp;</label>
-      <label><input type = "radio" name = "timeframe" value = "-1 day" />Last Day</label>
+      <label><input class = "right" type = "radio" name = "timeframe" value = "-1 day" />Last Day</label><br />
       <label><input type = "radio" name = "timeframe" value = "-1 week" />Last Week</label>
-      <label><input type = "radio" name = "timeframe" value = "-1 month" />Last Month</label><br />
+      <label><input class = "right" type = "radio" name = "timeframe" value = "-1 month" />Last Month</label><br />
       <input type = "submit" value = "Search" />
      </fieldset>
-		</form>
+    </form>
    </div> <!-- End Cell Div -->
    <b class="rbottom">
     <b class="r4"></b> <b class="r3"></b> <b class="r2"></b> <b class="r1"></b><b class="r0"></b>
@@ -66,10 +77,10 @@ if (empty($form_data['action'])) {
    <b class="rtop">
     <b class="r0"></b> <b class="r1"></b> <b class="r2"></b> <b class="r3"></b> <b class="r4"></b>
    </b>
-	<div class="cell">
+  <div class="cell">
    <div id = "results">
-	 </div> <!-- End Results Div -->
-	</div> <!-- End Cell Div -->
+   </div> <!-- End Results Div -->
+  </div> <!-- End Cell Div -->
     <b class="rbottom">
      <b class="r4"></b> <b class="r3"></b> <b class="r2"></b> <b class="r1"></b><b class="r0"></b>
     </b>
@@ -78,42 +89,42 @@ if (empty($form_data['action'])) {
     <p>Contact us and stuff
      <a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="./img/w3c-css.png" alt="Valid CSS!" height="31" width="88" /></a>
      <a href="http://validator.w3.org/check?uri=referer"><img src="./img/w3c-xhtml.png" alt="Valid XHTML 1.0 Transitional" height="31" width="88" /></a>
-	  </p>
+    </p>
    </div> <!-- End Footer Div -->
  </body>
 </html>
-			
+      
 
 <?php
 } else if ($form_data['action'] == "search") {
-	$output = "
-			<h1>Results:</h1>
-			<hr />";
-	$query = "Select * from Win32_NTLogEvent Where";
-	if (!isset($form_data['fqdn'])) {
-		$form_data['fqdn'] = ".";
-	};
-	if ($form_data['logfile'] != "all") {
-		$query .= " LogFile = '" . $form_data['logfile'] . "' and";
-	};
-	$query .= " TimeWritten >= '" . date('YmdHis.000000-000', strtotime($form_data['timeframe'])) . "'";
-	$objWMIService = new COM("winmgmts:{impersonationLevel=impersonate,(Security)}//" . $form_data['fqdn'] . "\\root\\cimv2");
-	$LoggedEvents = $objWMIService->ExecQuery($query);
-	foreach ($LoggedEvents as $objEvent) {
-		$output .= "Category: " . $objEvent->Category . "<br />";
-		$output .= "Computer Name: " . $objEvent->ComputerName . "<br />";
-		$output .= "Event Code: " . $objEvent->EventCode . "<br />";
-		$output .= "Log File: " . $objEvent->LogFile . "<br />";
-		$output .= "Message: " . $objEvent->Message . "<br />";
-		$output .= "Record Number: " . $objEvent->RecordNumber . "<br />";
-		$output .= "Source Name: " . $objEvent->SourceName . "<br />";
-		$output .= "Time Written: " . $objEvent->TimeWritten . "<br />";
-		$output .= "Event Type: " . $objEvent->Type . "<br />";
-		$output .= "User: " . $objEvent->User . "<br />";
-		$output .= "<hr />";
-	};
-	print $query . "<br />";
-	print $output;
+  $output = "
+      <h1>Results:</h1>
+      <hr />";
+  $query = "Select * from Win32_NTLogEvent Where";
+  if (!isset($form_data['fqdn'])) {
+    $form_data['fqdn'] = ".";
+  };
+  if ($form_data['logfile'] != "all") {
+    $query .= " LogFile = '" . $form_data['logfile'] . "' and";
+  };
+  $query .= " TimeWritten >= '" . date('YmdHis.000000-000', strtotime($form_data['timeframe'])) . "'";
+  $objWMIService = new COM("winmgmts:{impersonationLevel=impersonate,(Security)}//" . $form_data['fqdn'] . "\\root\\cimv2");
+  $LoggedEvents = $objWMIService->ExecQuery($query);
+  foreach ($LoggedEvents as $objEvent) {
+    $output .= "Category: " . $objEvent->Category . "<br />";
+    $output .= "Computer Name: " . $objEvent->ComputerName . "<br />";
+    $output .= "Event Code: " . $objEvent->EventCode . "<br />";
+    $output .= "Log File: " . $objEvent->LogFile . "<br />";
+    $output .= "Message: " . $objEvent->Message . "<br />";
+    $output .= "Record Number: " . $objEvent->RecordNumber . "<br />";
+    $output .= "Source Name: " . $objEvent->SourceName . "<br />";
+    $output .= "Time Written: " . $objEvent->TimeWritten . "<br />";
+    $output .= "Event Type: " . $objEvent->Type . "<br />";
+    $output .= "User: " . $objEvent->User . "<br />";
+    $output .= "<hr />";
+  };
+  print $query . "<br />";
+  print $output;
 };
 
 ?>
