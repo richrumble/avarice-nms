ea446e8c16346ba39f2bee0aecf6870abf9c1d49
phreakbag@gmail.com <phreakbag@gmail.com@1a33258e-1bfd-11df-83b7-07facc0237bd>
2012-12-14 03:28:46 +0000

    Beginnings of agent. Config created.

---
+++ avarice-nms/agent/agent.php
@@ -0,0 +1,14 @@
+<?php
+header("Content-Type: text/plain");
+
+date_default_timezone_set('UTC');
+
+$config       = json_decode(file_get_contents('./config.json'), TRUE);
+$runTimeEpoch = date('U');
+$timeZone     = date('e');
+print $timeZone . "\n\n";
+
+print date('Y-m-d H:i:s', $runTimeEpoch) . "\n\n";
+
+print_r($config);
+?>
