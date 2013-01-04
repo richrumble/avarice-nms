c26902caee501a2a1cc7da2dce66e499bfbfd262
phreakbag@gmail.com <phreakbag@gmail.com@1a33258e-1bfd-11df-83b7-07facc0237bd>
2013-01-04 04:22:14 +0000

    Fleshed out more agent runTime details. Fleshed out more eventLog runtime details.

--- trunk/avarice-nms/agent/agent.php
+++ trunk/avarice-nms/agent/agent.php
@@ -6,7 +6,6 @@ date_default_timezone_set('UTC');
 
 // Get the run time and calculalte the minute of the day
 $runTimeEpoch = date('U');
-$minuteOfDay  = (date('H', $runTimeEpoch) * 60) + (date('i', $runTimeEpoch));
 
 // Load config
 $config = json_decode(file_get_contents('./config.json'), TRUE);
@@ -21,24 +20,69 @@ if (!is_file($config['avariceDataFile']['path']))
 // connects to local database / creates it if need be
 try {
 	$avarice_dbh = new PDO("sqlite:" . $config['avariceDataFile']['path']);
-	// Need to create runLog, errorLog and loaded module default tables.
 }
 catch(PDOException $e)
 {
 	echo $e->getMessage();
 }
 
+// Creates default tables if this is the fist run
+if ($avarice_dbExists == FALSE)
+{
+	$query = "
+		CREATE TABLE agent_runLog
+		(
+			startTime TEXT,
+			endTime TEXT,
+			status TEXT,
+			modulesRan TEXT
+		);
+		CREATE TABLE agent_errorLog
+		(
+			pkID INTEGER PRIMARY KEY,
+			errorTime TEXT,
+			error TEXT
+		);
+		CREATE TABLE agent_module
+		(
+			moduleName TEXT UNIQUE,
+			version TEXT,
+			installDate TEXT,
+			lastRan TEXT
+		);";
+	$avarice_dbh->exec($query);
+}
+
+// List of modules that have ben installed (ran at least once)
+$modulesInstalled = array();
+$query = "
+	SELECT
+		moduleName,
+		lastRan
+	FROM agent_module;";
+foreach ($avarice_dbh->query($query) as $row)
+{
+	$modulesInstalled[$row['moduleName']] = $row['lastRan'];
+}
+		
 // Determines which modules to run based on the interval defined in the config
 $runModules = array();
-foreach ($config as $key => $value)
+foreach ($config['module'] as $key => $value)
 {
-	if (
-			(isset($value['intervalMinutes']))
-			and
-			($minuteOfDay % $value['intervalMinutes'] == 0)
-		)
+	if (!isset($value['disabled']) or $value['disabled'] == 0)
 	{
-		$runModules[] = $key;
+		if (in_array($key, array_keys($modulesInstalled)))
+		{
+			$minutes =  floor(($runTimeEpoch - date('U', strtotime($modulesInstalled[$key]))) / 60);
+			if ($minutes >= $value)
+			{
+				$runModules[] = $key;
+			}
+		}
+		else
+		{
+			$runModules[] = $key;
+		}
 	}
 }
 
@@ -47,5 +91,14 @@ foreach($runModules as $runModule)
 {
 	include_once("./module/" . $runModule . ".php");
 }
+
+// Need to create runLog, errorLog and loaded module default tables.
+$endEpoch = date('U');
+$query = "
+	INSERT INTO agent_runLog
+		(startTime, endTime, modulesRan)
+	VALUES
+		('" . date('Y-m-d H:i:s', $runTimeEpoch) . "', '" . date('Y-m-d H:i:s', $endEpoch) . "', '" . implode(",", $runModules) . "');";
+$avarice_dbh->exec($query);
 
 ?>
