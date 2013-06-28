89de841fce849291a6d6efa6b8f33cf940e386f1
phreakbag@gmail.com <phreakbag@gmail.com@1a33258e-1bfd-11df-83b7-07facc0237bd>
2013-06-28 04:12:55 +0000

    Submit is not working

---
+++ trunk/avarice-nms/agent/interface/eventlog.php
@@ -0,0 +1,99 @@
+<html>
+	<head>
+		<script type = "text/javascript" src = "jquery-2.0.2.js"></script>
+		<script type = "text/javascript">
+			$('#mainForm form').submit(function(){
+				var formData = $(this).serialize();
+				$.ajax({
+					type: "POST",
+					url: 'sqlite.ajax.php',
+					data: formData,
+					dataType: 'json',
+					success: function(jsonResult){
+						var items = [];
+						$.each(jsonResult['data'], function(computerName, eventLog, source, user, timeWritten, type, message) {
+							items.push('<tr><td>' + computerName + '</td><td>' + eventLog + '</td><td>' + source + '</td><td>' + user + '</td><td>' + timeWritten + '</td><td>' + type + '</td><td>' + message + '</td></tr>');
+						});
+						$('<table/>', {
+							'id': 'resultTable',
+							html: items.join('')
+						}).appendTo('#resultPane');
+					}
+				});
+				return false;
+			});
+		</script>
+	</head>
+	<body>
+		<form id = "mainForm">
+			<input type = "hidden" name = "section" value = "eventLog" />
+			<input type = "hidden" name = "action" value = "logSearch" />
+			<div id = "selectBoxes"></div>
+			<script type = "text/javascript">
+				$.getJSON('sqlite.ajax.php?section=eventLog&action=getUser', function(jsonResult) {
+					var items = [];
+					$.each(jsonResult['data'], function(key, val) {
+						if (key == 0)
+						{
+							items.push('<option value="' + key + '" selected>' + val + '</option>');
+						}
+						else
+						{
+							items.push('<option value="' + key + '">' + val + '</option>');
+						}
+					});
+					$('<select/>', {
+						'multiple': 'multiple',
+						'name': 'userID[]',
+						'id': 'selectUser',
+						'size': '3',
+						html: items.join('')
+					}).appendTo('#selectBoxes');
+				});
+				$.getJSON('sqlite.ajax.php?section=eventLog&action=getEventLog', function(jsonResult) {
+					var items = [];
+					$.each(jsonResult['data'], function(key, val) {
+						if (key == 0)
+						{
+							items.push('<option value="' + key + '" selected>' + val + '</option>');
+						}
+						else
+						{
+							items.push('<option value="' + key + '">' + val + '</option>');
+						}
+					});
+					$('<select/>', {
+						'multiple': 'multiple',
+						'name': 'eventLogID[]',
+						'id': 'selectEventLog',
+						'size': '3',
+						html: items.join('')
+					}).appendTo('#selectBoxes');
+				});
+				$.getJSON('sqlite.ajax.php?section=eventLog&action=getSource', function(jsonResult) {
+					var items = [];
+					$.each(jsonResult['data'], function(key, val) {
+						if (key == 0)
+						{
+							items.push('<option value="' + key + '" selected>' + val + '</option>');
+						}
+						else
+						{
+							items.push('<option value="' + key + '">' + val + '</option>');
+						}
+					});
+					$('<select/>', {
+						'multiple': 'multiple',
+						'name': 'sourceID[]',
+						'id': 'selectSource',
+						'size': '3',
+						html: items.join('')
+					}).appendTo('#selectBoxes');
+				});
+			</script>
+			<input size = 30 type = "text" id = "searchString" name = "searchString" onkeyup="$('#mainForm form').submit();"/>
+			<input type = "submit" value = "clickme" />
+		</form>
+		<div id = "resultPane"></div>
+	</body>
+</html>
